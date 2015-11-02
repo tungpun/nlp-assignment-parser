cffebb35363b72ef4eba270d5518fd2cb94309cc
tungpun <htung.nht@gmail.com>
2015-11-02 02:54:53 +0000

    encoded utf-8 and included more stuff

--- doit.py
+++ doit.py
@@ -1,13 +1,41 @@
 #!/usr/bin7 python 2.7
+# -*- coding: utf-8 -*-
 
 import os
 import xmltodict
 import json
 import re
+import random
 
 DEBUG = True
 JAVABUFFER = '-Xmx2000m'
 INPUTFILE = 'input.txt'
+
+
+def basic_color(code):    
+    def inner(text, bold=True):
+        c = code
+        if bold:
+            c = "1;%s" % c
+        return "\033[%sm%s\033[0m" % (c, text)
+    return inner
+
+
+def term_color(code):    
+    def inner(text):
+        c = code
+        return "\033[38;5;%sm%s\033[0m" % (c, text)
+    return inner
+    
+    
+black = basic_color('30')
+red = basic_color('31')
+green = basic_color('32')
+yellow = basic_color('33')
+blue = basic_color('34')
+magenta = basic_color('35')
+cyan = basic_color('36')
+tred = term_color('31')
 
 
 def check_requirement():
@@ -21,19 +49,23 @@ def print_input():
     try:
         with open('input.txt', 'r') as f:
             data = f.read()
-            print '[+] Input Data: \"' + data.strip() + '\"\n'        
+            print cyan('[+] Input Data: ') + data.strip()[:200] + '...\n'        
         with open('input.txt.xml', 'w') as f:
             f.write("")
-            print '[+] Outfile cleaned:', INPUTFILE + '.xml\n'        
-    except:
+            print cyan('[+] Outfile cleaned:'), INPUTFILE + '.xml\n'        
+    except Exception, e:
+        if DEBUG:
+            print e 
         raise Exception("IO Error! Check input file!")
 
 
 def run(cmd):
     try:
-        print cmd.split(' ')
+        #print cmd.split(' ')
         os.popen(cmd)
-    except:
+    except Exception, e:
+        if DEBUG:
+            print e 
         raise Exception("Error when try running ", cmd)
 
 
@@ -41,11 +73,13 @@ def lex_parser():
     """Get input from INPUTFILE
     """
     try:
-        print "[+] Implementing Lexiclized Parser (included Dependency and Context-Free-Grammar representation)...\n"
+        print cyan("[+] Implementing Lexiclized Parser (included Dependency and Context-Free-Grammar representation)...\n")
         cmd = 'java -cp "*" ' + JAVABUFFER + ' edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,parse -file ' + INPUTFILE    
         run(cmd)  
-        print "[+] Parsing completed\n"
-    except:
+        print cyan("[+] Parsing completed\n")
+    except Exception, e:
+        if DEBUG:
+            print e 
         raise Exception('Failed. Quit now!!!')
     return 0
 
@@ -54,12 +88,14 @@ def shift_reduce():
     """Get input from INPUTFILE
     """
     try:
-        print "[+] Implementing Shift Reduce Parser (included Dependency and Context-Free-Grammar representation)...\n"
+        print cyan("[+] Implementing Shift Reduce Parser (included Dependency and Context-Free-Grammar representation)...\n")
         model = 'edu/stanford/nlp/models/srparser/englishSR.ser.gz'
         cmd = 'java -cp "*" ' + JAVABUFFER + ' edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,parse -parse.model ' + model + ' -file ' + INPUTFILE            
         run(cmd)  
-        print "[+] Parsing completed\n"
-    except:
+        print cyan("[+] Parsing completed\n")
+    except Exception, e:
+        if DEBUG:
+            print e 
         raise Exception('Failed. Quit now!!!')
     return 0
 
@@ -68,12 +104,14 @@ def neural_network_parser():
     """Get input from INPUTFILE
     """
     try:
-        print "[+] Implementing Neural Network Parser (included Dependency representation)...\n"
+        print cyan("[+] Implementing Neural Network Parser (included Dependency representation)...\n")
         model = 'edu/stanford/nlp/models/parser/nndep/english_UD.gz'
         cmd = 'java -cp "*" ' + JAVABUFFER + ' edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,depparse -parse.model ' + model + ' -file ' + INPUTFILE            
         run(cmd)  
-        print "[+] Parsing completed\n"
-    except:
+        print cyan("[+] Parsing completed\n")
+    except Exception, e:
+        if DEBUG:
+            print e 
         raise Exception('Failed. Quit now!!!')
     return 0
 
@@ -82,12 +120,14 @@ def vietnamese_pcfg():
     """Get input from INPUTFILE
     """
     try:
-        print "[+] Implementing Vietnamese PCFG...\n"
+        print cyan("[+] Implementing Vietnamese PCFG...\n")
         model = 'edu/stanford/nlp/models/parser/nndep/english_UD.gz'
-        cmd = 'java -cp "*" ' + JAVABUFFER + ' edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,depparse -parse.model VietNamesePCFG -file ' + INPUTFILE            
+        cmd = 'java -cp "*" ' + JAVABUFFER + ' edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,parse -parse.model VietNamesePCFG -file ' + INPUTFILE          
         run(cmd)  
-        print "[+] Parsing completed\n"
-    except:
+        print cyan("[+] Parsing completed\n")
+    except Exception, e:
+        if DEBUG:
+            print e 
         raise Exception('Failed. Quit now!!!')
     return 0
 
@@ -122,8 +162,8 @@ def gen_graph(line):
 
     line = line.replace('  ', ' ')
     line = line.replace(') ', ')')    
-    line = line.replace(' (', '(')
-    print "\n", line
+    line = line.replace(' (', '(')        
+    #print "\n", line
     tabsize = 0
     graph = ''
     line += '~'
@@ -153,35 +193,53 @@ def read_output(cmd):
     """Read output report from INPUTFILE.xml
     """  
     OUTPUTFILE = INPUTFILE + '.xml'    
+
     try:
         with open(OUTPUTFILE, 'r') as f:
+
             data = f.read()        
+
             if data == "":
                 raise Exception("Outfile is empty ! Check log and report to developer!!!")        
-            print '[+] Output report is saved to', OUTPUTFILE, '. You can open with MS Excel for more detail or view a brief as below.'
+            
+            print cyan('[+] Output report is saved to' + OUTPUTFILE + '. You can open with MS Excel for more detail or view a brief as below.')
             if cmd == "nn":
                 return 0
-            print "\n[+] Parse tree:"
+            print cyan("\n[+] Parse tree:")
+
             try:
-                parsetrees = get_parse_tree(data)            
+                parsetrees = get_parse_tree(data)                   
                 cnt = 0
                 for parsetree in parsetrees:            
+                    parsetree = parsetree.encode('utf-8')
+                    #print '   [' + str(cnt) + ']', parsetree.encode('utf-8'), '\n'
                     print '   [' + str(cnt) + ']', parsetree, gen_graph(parsetree), '\n'
                     cnt += 1
-            except:
+            except Exception, e:
+                if DEBUG:
+                    print e
                 print "     not available..."
                 pass
-    except:
+
+    except Exception, e:
+        if DEBUG:
+            print e        
         raise Exception("Something wrongs ! Check log and report to developer!!!")
+
     return 0
 
 
-if __name__ == '__main__':    
-    check_requirement()
-    if DEBUG:
-        print_input()    
-    promt_message = """
-       
+promt_message = cyan("""\n    Select from the menu:""") + blue("""
+        [1] Lexiclized Parser
+        [2] Shift Reduce Parser
+        [3] Neural Network Parser
+        [4] Vietnamese PCFG
+        [0] Quit Parser Wrappe\n\n""") + green("""    Parser Wrapper> """)
+
+def print_cover():
+    os.system('clear')
+    raw = ("""
+        
          .d8888b.                                      .d8888b.                      .d8888b.         d8888 
         d88P  Y88b                                    d88P  Y88b                    d88P  Y88b       d88888 
         888    888                                         .d88P                    888    888      d88P888 
@@ -193,15 +251,18 @@ if __name__ == '__main__':
                                              888                                                            
                                              888                            | Neutral Language Processing               
                                              888                            | Parser Wrapper               
+    """).split('\n')
+    
+    for line in raw:
+        color = basic_color(str(random.randint(31, 36)))
+        print color(line)
+    
 
-    Select from the menu:
-        [1] Lexiclized Parser
-        [2] Shift Reduce Parser
-        [3] Neural Network Parser
-        [4] Vietnamese PCFG
-        [0] Quit Parser Wrapper
-
-    Parser Wrapper> """
+if __name__ == '__main__':    
+    print_cover()
+    check_requirement()
+    if DEBUG:
+        print_input()    
     while (True):
         cmd = raw_input(promt_message).strip()
         if cmd == '2':
@@ -219,5 +280,5 @@ if __name__ == '__main__':
         elif cmd == '0':
             quit()
         else:
-            print "Try again! Type correct input, please!"    
+            print blue("Try again! Type correct input, please!")
 
